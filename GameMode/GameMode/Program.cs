b7fce4ab8482d91a70e6cc42605fd6c0a9745fc2
Tyom <tyom58@gmail.com>
2020-03-31 01:07:19 +0400

    first commit

---
+++ GameMode/GameMode/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.Win32;
+namespace GameMode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           SwitchHotKey();
+        }
+
+        static public void SwitchHotKey()
+        {
+            try
+            {
+                const string keyName = @"HKEY_CURRENT_USER\Keyboard Layout\Toggle";
+                if ("1" == Registry.GetValue(keyName, "Hotkey", null).ToString())
+                {
+                    Registry.SetValue(keyName, "Hotkey", "3");
+                    Registry.SetValue(keyName, "Language Hotkey", "3");
+                }
+                else if ("3" == Registry.GetValue(keyName, "Hotkey", null).ToString())
+                {
+                    Registry.SetValue(keyName, "Hotkey", "1");
+                    Registry.SetValue(keyName, "Language Hotkey", "1");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+    
+        }
+    }
+}
